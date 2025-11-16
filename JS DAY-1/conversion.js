80053a9b6769004e57b9ff7b8eb638c24f81d54d
CoderCat010 <nusraumme@gmail.com>
2025-11-16 14:39:59 +0600

    added file1

---
+++ JS DAY-1/conversion.js
@@ -0,0 +1,15 @@
+/***TYPES OF JS CONVERSION
+ * 1. Manual conversion ------ we can convert into other data type 
+ * 2. automatic conversion ------ js by dafault convert into other data type
+ */
+
+//? Manual conversion
+// let num = 22 + 'hi';
+// console.log(num);
+
+/*------------------*/
+// let num = 22 - '10'; // , * , / করলে String → Number হয়ে যায়। except + karon plus mane string number jora lagno r baki operation gulor khetre string to jora lagano jay na tai real valu ta dey orthat string ke number e convert kore dey.
+// console.log(num);
+
+// ?automatic conversion
+let num = 13;
---
+++ JS DAY-1/practice-1.js
@@ -0,0 +1,19 @@
+//* ======JavaScript conditional statedment=====
+
+// ?if
+// let age = 14;
+// if (age < 18){
+//     console.log('you are teenager');
+// }
+
+// ? if else
+    // let age = 133
+    // if (age >= 18){
+    //     console.log('you are adult');
+    // }
+    // else{
+    //     console.log('you are teenage')
+    // }
+
+// ! if...if else....else
+
