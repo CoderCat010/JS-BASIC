61f006a19745460fc2ae9fad8e6bb5bb4e817f83
CoderCat010 <nusraumme@gmail.com>
2025-11-16 15:02:04 +0600

    added parsefloat, parseint practice task

--- JS DAY-1/conversion.js
+++ JS DAY-1/conversion.js
@@ -3,7 +3,7 @@
  * 2. automatic conversion ------ js by dafault convert into other data type
  */
 
-//? Manual conversion
+//?automatic conversion
 // let num = 22 + 'hi';
 // console.log(num);
 
@@ -11,7 +11,7 @@
 // let num = 22 - '10'; // , * , / করলে String → Number হয়ে যায়। except + karon plus mane string number jora lagno r baki operation gulor khetre string to jora lagano jay na tai real valu ta dey orthat string ke number e convert kore dey.
 // console.log(num);
 
-// ?automatic conversion
+// ?Manual conversion 
 
 /** string to number */
 // let strTonum = '13';
@@ -20,5 +20,12 @@
 // /** number to string */
 // let numTostr = 13;
 // console.log(String(numTostr));
+
+/** number to parsefloat, parseint*/
+// let Parsefloat = '13.4884384 year';
+// console.log(parseFloat(Parsefloat));
+
+// let parseIntiger = 23.55
+// console.log(parseInt(parseIntiger));
 
 
