c67391ed62c72a7863ad9e050ea0667a8a40a119
CoderCat010 <nusraumme@gmail.com>
2025-11-17 11:06:47 +0600

    added practice-2

--- JS DAY-2/conditional-statement.js
+++ JS DAY-2/conditional-statement.js
@@ -1,6 +1,16 @@
 // একটি সংখ্যা পজিটিভ হলে “Positive” প্রিন্ট করো।
-// var positiveNum = 3
+// var positiveNum = 3;
+
 // if(positiveNum > 0){
 //     console.log('positive');
 // }      
 
+// সংখ্যা ১০ এর বেশি হলে “Greater than 10” দেখাও, সংখ্যা ১০ এর কম হলে “Less than 10” দেখাও।
+var greaterThanLessthan = 33;
+
+if(greaterThanLessthan > 10){
+    console.log('greater than 10');
+}
+else{
+    console.log('less than 10');
+}
