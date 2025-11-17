c5785996aafe725fe7ea7628bb295745d4d565c8
CoderCat010 <nusraumme@gmail.com>
2025-11-17 11:18:12 +0600

    added practice-3

--- JS DAY-2/conditional-statement.js
+++ JS DAY-2/conditional-statement.js
@@ -5,12 +5,27 @@
 //     console.log('positive');
 // }      
 
-// সংখ্যা ১০ এর বেশি হলে “Greater than 10” দেখাও, সংখ্যা ১০ এর কম হলে “Less than 10” দেখাও।
-var greaterThanLessthan = 33;
 
-if(greaterThanLessthan > 10){
-    console.log('greater than 10');
-}
-else{
-    console.log('less than 10');
-}
+// সংখ্যা ১০ এর বেশি হলে “Greater than 10” দেখাও, সংখ্যা ১০ এর কম হলে “Less than 10” দেখাও।
+// var greaterThanLessthan = 33;
+
+// if(greaterThanLessthan > 10){
+//     console.log('greater than 10');
+// }
+// else{
+//     console.log('less than 10');
+// }
+
+
+/**
+ * একটি সংখ্যা জোড় হলে “Even” দেখাও।
+ * একটি সংখ্যা বিজোড় হলে “Odd” দেখাও।
+*/
+// var evenOrodd = 32;
+
+// if(evenOrodd % 2 == 0){
+//     console.log('even');
+// }
+// else{
+//     console.log('odd');
+// }
