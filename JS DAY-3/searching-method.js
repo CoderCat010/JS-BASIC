77668b9d71bbccc8675ccada82374af532ab8fbb
CoderCat010 <nusraumme@gmail.com>
2025-11-19 01:44:17 +0600

    added practice 2

--- JS DAY-3/searching-method.js
+++ JS DAY-3/searching-method.js
@@ -1,3 +1,11 @@
-var searchElemenet = 'i will always win!';
-var searching = searchElemenet.includes('oo');
-console.log(searching);
+// var searchElemenet = 'i will always win!';
+// var searching = searchElemenet.includes('oo');
+// console.log(searching);
+
+
+// var searchElemenet = 'i will always win!';
+// var searching = searchElemenet.includes('i', 3);
+// console.log(searching);
+
+
+
