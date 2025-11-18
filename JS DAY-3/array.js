71b972f0a67715353a185d0aaf8e27786b9ac68f
CoderCat010 <nusraumme@gmail.com>
2025-11-18 23:26:28 +0600

    added practice 4

--- JS DAY-3/array.js
+++ JS DAY-3/array.js
@@ -12,11 +12,21 @@
 // console.log(additemslast);
 
 
-var addItemsfromFirst = [44, 66, 22, 3, 8];
-var addnewItemsfromFirst = addItemsfromFirst.unshift(2, 6);
+// var addItemsfromFirst = [44, 66, 22, 3, 8];
+// var addnewItemsfromFirst = addItemsfromFirst.unshift(2, 6);
+// console.log(addnewItemsfromFirst);
+// console.log(addItemsfromFirst);
 
-console.log(addnewItemsfromFirst);
-console.log(addItemsfromFirst);
+
+// var addItemsfromAnywhere = ['gg', 'hh', 'll'];
+// var addnewItemsfromAnywhere = addItemsfromAnywhere.splice(1, 1, 'dd', 'jj');
+// console.log(addnewItemsfromAnywhere);
+// console.log(addItemsfromAnywhere);
+
+
+
+
+
 
 
 
