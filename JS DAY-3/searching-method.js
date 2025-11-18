7a49a157e192b2c6173f4e88591cf2f1beb48f78
CoderCat010 <nusraumme@gmail.com>
2025-11-19 02:20:02 +0600

    added practice 3

--- JS DAY-3/searching-method.js
+++ JS DAY-3/searching-method.js
@@ -8,4 +8,10 @@
 // console.log(searching);
 
 
+var searchElemenet = [2, 4, 6, 8, 7, 6];
+// var searching = searchElemenet.indexOf(4);
+var searching1 = searchElemenet.indexOf(6, 3);
+console.log(searching1);
+
+
 
