c943f3d89a3a5c6496f1b5a461d77edd493df970
CoderCat010 <nusraumme@gmail.com>
2025-11-19 03:06:24 +0600

    added practice 7

--- JS DAY-3/join-convert.js
+++ JS DAY-3/join-convert.js
@@ -1,3 +1,7 @@
-var joining = [4, 6, 7, 8];
-var joinThis = joining.join('');
-console.log(joinThis);
+// var joining = [4, 6, 7, 8];
+// var joinThis = joining.join('');
+// console.log(joinThis);
+
+var converting = [2, 4, 6];
+console.log(converting.toString());
+
