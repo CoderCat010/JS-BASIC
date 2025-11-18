94248052ecc2eeee50bdd828ab67c2757fb0eb4a
CoderCat010 <nusraumme@gmail.com>
2025-11-18 21:56:23 +0600

    added practice

--- JS DAY-3/array.js
+++ JS DAY-3/array.js
@@ -1,2 +1,6 @@
-var arrayElm = [4, 5, 7, 8, 8, 8];
-console.log(arrayElm);
+// var arrayElm = [4, 5, 7, 8, 8, 8];
+// console.log(arrayElm);
+
+var arrayStr = ['jj', 'njjn', 'jnjn'];
+console.log(arrayStr);
+
