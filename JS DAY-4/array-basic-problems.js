4f37bad06ba6f0bd694142ab4ad033dedcf69b0a
CoderCat010 <nusraumme@gmail.com>
2025-11-19 19:17:46 +0600

    added practice-2

--- JS DAY-4/array-basic-problems.js
+++ JS DAY-4/array-basic-problems.js
@@ -1,8 +1,11 @@
 //? একটি array এর প্রথম উপাদান বের করো
-var getElement = ['f', 'g', 'j'];
-console.log(getElement[0]);
+// var getElement = ['f', 'g', 'j'];
+// console.log(getElement[0]);
 
 
+// একটি array এর length বের করো
+// var getLength = [1, 2, 3, 4, 5, 6];
+// console.log(getLength.length);
 
 
 
