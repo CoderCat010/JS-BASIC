2d90310eec7e36736d352f80d1b69479489d70c7
CoderCat010 <nusraumme@gmail.com>
2025-11-22 04:54:35 +0600

    added practice-11

--- JS DAY-6/loop-practice-problems.js
+++ JS DAY-6/loop-practice-problems.js
@@ -3,3 +3,10 @@
 // for(let items of getElm){
 //     console.log(items);
 // }
+
+
+// array-এর সব element reverse order-এ print করো।
+let getElm = [3, 5, 6, 7, 9];
+for(let i = getElm.length - 1; i >= 0; i--){
+    console.log(getElm[i]);
+}
