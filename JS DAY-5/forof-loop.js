fa5da381c0a5e6abbdae2d64935321ea0eb5a1ce
CoderCat010 <nusraumme@gmail.com>
2025-11-22 03:43:42 +0600

    added practice-9

--- JS DAY-5/forof-loop.js
+++ JS DAY-5/forof-loop.js
@@ -1,4 +1,13 @@
-let nums = [1, 3,5, 6, 6];
+// let nums = [1, 3,5, 6, 6];
+// for(let item of nums){
+//     console.log(item);
+// }
+
+
+let nums = [1, 5, 6, 3, 7];
 for(let item of nums){
     console.log(item);
+    if(item === 6){
+        break;
+    }
 }
