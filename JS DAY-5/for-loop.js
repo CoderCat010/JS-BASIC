1c2229d82830a7fca4956ec5b3ac0eb2aa644965
CoderCat010 <nusraumme@gmail.com>
2025-11-21 17:25:12 +0600

    added practice-7

--- JS DAY-5/for-loop.js
+++ JS DAY-5/for-loop.js
@@ -12,3 +12,20 @@
 // for(let i = 0; i < str.length; i++){
 //     console.log(str[i]);
 // }
+
+
+// let nums = [2, 4, 5, 2, 6];
+// let sumOfeven = 0;
+// let sumOfodd = 0;
+
+// for(let i = 0; i < nums.length; i++){
+//     const x = nums[i]; 
+//     if(x % 2 === 0){
+//       sumOfeven += x; 
+//     }else{
+//         sumOfodd += x;
+//     }
+// }
+// console.log(sumOfeven);
+// console.log(sumOfodd);
+
