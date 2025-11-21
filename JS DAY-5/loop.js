5fb8ffc59730a33f87ecb2070e951a34e40910ce
CoderCat010 <nusraumme@gmail.com>
2025-11-21 16:31:03 +0600

    added practice-2

--- JS DAY-5/loop.js
+++ JS DAY-5/loop.js
@@ -1,7 +1,18 @@
-//while loop
+// while loop
+// var i = 1;
+// while(i <= 5){
+//     console.log('hehe');
+//     i++;
+// }
 
-var i = 1;
-while(i <= 5){
-    console.log('hehe');
-    i++;
+
+let i= 1;
+let sum = 0;
+while(i <= 10){
+   sum += i;
+   i++ 
+//    console.log(sum);
 }
+console.log(sum);
+
+
