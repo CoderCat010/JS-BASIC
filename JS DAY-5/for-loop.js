03f10003fb987e649e13c55e3ffe9115a18ce945
CoderCat010 <nusraumme@gmail.com>
2025-11-21 16:47:14 +0600

    added practice-5

--- JS DAY-5/for-loop.js
+++ JS DAY-5/for-loop.js
@@ -1,3 +1,8 @@
-for(let i = 1; i <=10; i +=2){
-    console.log(i);
+// for(let i = 1; i <=10; i +=2){
+//     console.log(i);
+// }
+
+
+for(let i= 1; i<=10; i++){
+    console.log('hi!...', i);
 }
