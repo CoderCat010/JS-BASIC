b2922fc87addda6de8b14c662c3e065de3502d27
CoderCat010 <nusraumme@gmail.com>
2025-11-23 00:36:57 +0600

    added practice-3

--- JS DAY-6/loop-practice-problems.js
+++ JS DAY-6/loop-practice-problems.js
@@ -6,7 +6,18 @@
 
 
 // array-এর সব element reverse order-এ print করো।
-let getElm = [3, 5, 6, 7, 9];
-for(let i = getElm.length - 1; i >= 0; i--){
-    console.log(getElm[i]);
+// let getElm = [3, 5, 6, 7, 9];
+// for(let i = getElm.length - 1; i >= 0; i--){
+//     console.log(getElm[i]);
+// }
+
+
+// array-এর সব সংখ্যার যোগফল বের করো।
+var addingElem = [1, 3, 4, 5, 6];
+var getElm = 0;
+
+for(let i=  0; i < addingElem.length; i++){
+    getElm += addingElem[i];
+    // console.log(getElm);
 }
+// console.log(getElm);
