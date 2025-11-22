1e20690c36e11c6e468f823f9669879f3f5e8619
CoderCat010 <nusraumme@gmail.com>
2025-11-23 00:50:20 +0600

    added practice-4

--- JS DAY-6/loop-practice-problems.js
+++ JS DAY-6/loop-practice-problems.js
@@ -13,11 +13,25 @@
 
 
 // array-এর সব সংখ্যার যোগফল বের করো।
-var addingElem = [1, 3, 4, 5, 6];
-var getElm = 0;
+// var addingElem = [1, 3, 4, 5, 6];
+// var getElm = 0;
 
-for(let i=  0; i < addingElem.length; i++){
-    getElm += addingElem[i];
-    // console.log(getElm);
+// for(let i=  0; i < addingElem.length; i++){
+//     getElm += addingElem[i];
+//     // console.log(getElm);
+// }
+// // console.log(getElm);
+
+
+/**
+ * array-এর সর্বোচ্চ সংখ্যা খুঁজে বের করো।
+ * array-এর সর্বনিম্ন সংখ্যা খুঁজে বের করো।
+**/
+var getElem = [1, 3, 4, 5, 6];
+var maxElem = 0;
+for(let i = 0; i < getElem.length; i++){
+    if(getElem[i] > maxElem){
+        maxElem = getElem[i];
+    }
 }
-// console.log(getElm);
+console.log(maxElem);
