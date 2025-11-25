9ca8a9f3d81120f7571013089e1ade5861cc6c04
CoderCat010 <nusraumme@gmail.com>
2025-11-25 09:46:34 +0600

    added practice5

--- JS DAY-1/string.js
+++ JS DAY-1/string.js
@@ -1,14 +1,23 @@
 // let str = 'amar sonar bangla'
 // console.log(str);
 
+
 // // length 
 // let getStrLength = 'fnjehfuehfncufhrchmh';
 // console.log(getStrLength.length);
 
-// Case Conversion
-let MakeupperCase = 'vjfvvjnvj';
-console.log(MakeupperCase.toUpperCase());
 
-let MakelowerCase = 'NDJFHDFJD';
-console.log(MakelowerCase.toLowerCase());
+// // Case Conversion
+// let MakeupperCase = 'vjfvvjnvj';
+// console.log(MakeupperCase.toUpperCase());
+
+// let MakelowerCase = 'NDJFHDFJD';
+// console.log(MakelowerCase.toLowerCase());
+
+
+// Searching Methods
+// indexof ---- kono string er vitore jekono substring er position khuje ber kora
+let getStr = 'jjfkkdvfvfvfk';
+console.log(getStr.indexOf('f'));
+console.log(getStr.indexOf('k', 6));
 
