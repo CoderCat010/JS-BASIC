54a9d2a6fd77aa49f32f59026fbd1cfab440f0ff
CoderCat010 <nusraumme@gmail.com>
2025-11-26 13:11:40 +0600

    added practice-12

--- JS DAY-1/modifying-methods.js
+++ JS DAY-1/modifying-methods.js
@@ -4,6 +4,11 @@
 // console.log(str1.concat(str2));  
 
 
-// replace
-let str1 = 'hello world';
-console.log(str1.replace('world', 'javascript'));
+// // replace
+// let str1 = 'hello world';
+// console.log(str1.replace('world', 'javascript'));
+
+
+// // trim
+// let str1 = '   hello   ';
+// console.log(str1.trim());
