13c570598efd986260d7471e7dcfe885e75df4bb
CoderCat010 <nusraumme@gmail.com>
2025-12-11 18:01:48 +0600

    added practice-6

--- JS DAY-2/number-methods.js
+++ JS DAY-2/number-methods.js
@@ -9,6 +9,7 @@
 // console.log(NumtoStr.toString());
 
 
+// // isFinite()
 // Number.isFinite(value)
 // let a = 123;
 // let b = Infinity;
@@ -16,4 +17,4 @@
 // console.log(isFinite(a));
 // console.log(isFinite(b));
 // console.log(isFinite(c));
-// console.log(Number.isFinite(c)); // ekhane type check korce but number e convert korce na etai niom je jokhon amra type check korbo tokhon amake Number.isFinite use korte hobe.r jokhon amara type check korbo na tokhon amra isFinite use korte parbo tokhon true dekhabe karon isfinite automatic type conversion kore ney.
+// console.log(Number.isFinite(c)); // // ekhane type check korce but number e convert korce na etai niom je jokhon amra type check korbo tokhon amake Number.isFinite use korte hobe.r jokhon amara type check korbo na tokhon amra isFinite use korte parbo tokhon true dekhabe karon isfinite automatic type conversion kore ney.
