59a46b24941635ca8f104c55d3d1e79dca399362
CoderCat010 <nusraumme@gmail.com>
2025-12-11 17:59:56 +0600

    added practice-66

--- JS DAY-2/number-methods.js
+++ JS DAY-2/number-methods.js
@@ -4,6 +4,16 @@
 // console.log(Number(num.toFixed(2)));
 
 
-// toString()
-const NumtoStr = 255;
-console.log(NumtoStr.toString());
+// // toString()
+// const NumtoStr = 255;
+// console.log(NumtoStr.toString());
+
+
+// Number.isFinite(value)
+// let a = 123;
+// let b = Infinity;
+// let c = '123';  
+// console.log(isFinite(a));
+// console.log(isFinite(b));
+// console.log(isFinite(c));
+// console.log(Number.isFinite(c)); // ekhane type check korce but number e convert korce na etai niom je jokhon amra type check korbo tokhon amake Number.isFinite use korte hobe.r jokhon amara type check korbo na tokhon amra isFinite use korte parbo tokhon true dekhabe karon isfinite automatic type conversion kore ney.
