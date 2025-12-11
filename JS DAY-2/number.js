3a3c2462e98c5c5b42a1fcea438c34eb0cc5d862
CoderCat010 <nusraumme@gmail.com>
2025-12-11 15:52:02 +0600

    added practice-2

--- JS DAY-2/number.js
+++ JS DAY-2/number.js
@@ -11,3 +11,9 @@
 // // console.log(x + y); // adding two string's value together
 // // console.log(Number(x) + Number(y)); converting into number and then adding real value
 // // console.log(+x + +y); // unary plus operator to convert string to number and then adding real value
+
+
+// let x = 10;
+// let y = 20;
+// let z = '30';
+// console.log(x + y + z); 
