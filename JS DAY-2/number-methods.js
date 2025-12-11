4535ef58b037acea59ba06421edd5cc209c5700f
CoderCat010 <nusraumme@gmail.com>
2025-12-11 17:43:18 +0600

    added practice-5

--- JS DAY-2/number-methods.js
+++ JS DAY-2/number-methods.js
@@ -1,3 +1,9 @@
 // // toFixed(digits)
 // const num = 123.456789;
 // console.log(num.toFixed(2));
+// console.log(Number(num.toFixed(2)));
+
+
+// toString()
+const NumtoStr = 255;
+console.log(NumtoStr.toString());
