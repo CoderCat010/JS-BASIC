85fbb3c44e5ab81e4832a6476f088bd59e6caa68
CoderCat010 <nusraumme@gmail.com>
2025-12-12 01:18:42 +0600

    added practice-1

---
+++ JS DAY-3/condition.js
@@ -0,0 +1,9 @@
+// const isSalary = 50000;
+// const isBCS = true;
+// const height = 185;
+
+// if(isSalary > 30000 && isBCS == true && height >= 180){
+//     console.log('cholo botsho biye kore feli!!!');
+// }else{
+//     console.log('ami biya korum naaaaa!!!');
+// }
