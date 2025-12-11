ca1a89043780a7e1454ffd31f4f77c400052adf9
CoderCat010 <nusraumme@gmail.com>
2025-12-11 13:17:20 +0600

    added practice-1

---
+++ JS DAY-2/number.js
@@ -0,0 +1,13 @@
+// let x = 123e5; 
+// console.log(x); 
+
+
+// let y = 123e-5;
+// console.log(y);
+
+
+// let x = '5';
+// let y = '5';
+// // console.log(x + y); // adding two string's value together
+// // console.log(Number(x) + Number(y)); converting into number and then adding real value
+// // console.log(+x + +y); // unary plus operator to convert string to number and then adding real value
