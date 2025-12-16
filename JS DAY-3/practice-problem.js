3963f15c000620eb5cb3d2f0d523004a9e8b85bb
CoderCat010 <nusraumme@gmail.com>
2025-12-16 13:49:32 +0600

    added practice-5

--- JS DAY-3/practice-problem.js
+++ JS DAY-3/practice-problem.js
@@ -24,20 +24,50 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - BMI >=25 and BMI <= 29.9, you are overweight.
     - Otherwise, you are obese.
 */
-let weight = 80;
-let height = 150;
-let converHeight = height / 100;
-let BMI = weight / (converHeight ** 2);
-if(BMI > 0){
-    if(BMI < 18.5){
-        console.log('you are underweight');
-    } else if(BMI >= 18.5 && BMI <=24.9){
-        console.log('you are normal');
-    } else if(BMI >=25 && BMI <= 29.9){
-        console.log('you are overweight');
-    }else{
-    console.log('you are obese');
-}
-}else{
-    console.log('invalid');
-}
+// let weight = 80;
+// let height = 150;
+// let converHeight = height / 100;
+// let BMI = weight / (converHeight ** 2);
+// if(BMI > 0){
+//     if(BMI < 18.5){
+//         console.log('you are underweight');
+//     } else if(BMI >= 18.5 && BMI <=24.9){
+//         console.log('you are normal');
+//     } else if(BMI >=25 && BMI <= 29.9){
+//         console.log('you are overweight');
+//     }else{
+//     console.log('you are obese');
+// }
+// }else{
+//     console.log('invalid');
+// }
+
+
+
+/***
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+// let myScore = 80;
+// let myFriendScore = 90;
+// if(myScore > 80){
+//     if(myFriendScore > 80){
+//         console.log('lets go for lunch!');
+//     }
+//     else if(myFriendScore < 80 && myFriendScore >= 60){
+//         console.log('good luck next time.');
+//     } else if(myFriendScore < 60 && myFriendScore >= 40){
+//         console.log('tata bye');
+//     } else if(myFriendScore < 40){
+//         console.log('im blocking you');
+//     }
+// }else{
+//     console.log('eeee eeee eeeee');
+// }
