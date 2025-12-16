4c93d204c94ec59fcf6ea7e904a20cb71be5a11f
CoderCat010 <nusraumme@gmail.com>
2025-12-16 12:46:09 +0600

    added practice-1

---
+++ JS DAY-3/logical-not.js
@@ -0,0 +1,6 @@
+let isSheComing = false;
+if(!isSheComing){
+    console.log('nachbo hele dule!');
+}else{
+    console.log('nah ghumaiiii');
+}
--- JS DAY-3/multiple-condition.js
+++ JS DAY-3/multiple-condition.js
@@ -1,8 +1,33 @@
-const price = 3000;
-if (price >= 5000){
-    const discount = price * 10 / 100;
-    const finalPrice = price - discount;
-    console.log(finalPrice);
-}else{
-    console.log(price);
-}
+// const price = 7000;
+// if (price >= 5000){
+//     const discount = price * 10 / 100;
+//     const finalPrice = price - discount;
+//     console.log(finalPrice);
+// }else{
+//     console.log(price);
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
