e308fb83ecd9bd9e4252beb7c8580243a9b9798c
CoderCat010 <nusraumme@gmail.com>
2025-12-16 12:50:11 +0600

    added practice-2

--- JS DAY-3/logical-not.js
+++ JS DAY-3/logical-not.js
@@ -1,6 +1,13 @@
-let isSheComing = false;
-if(!isSheComing){
-    console.log('nachbo hele dule!');
+// let isSheComing = false;
+// if(!isSheComing){
+//     console.log('nachbo hele dule!');
+// }else{
+//     console.log('nah ghumai!!!');
+// }
+
+let passed = false;
+if(!!passed){
+    console.log('ENGINEERING PORBO');
 }else{
-    console.log('nah ghumaiiii');
+    console.log('MURI KHA');
 }
