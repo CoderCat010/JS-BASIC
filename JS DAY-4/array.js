2fe10e28df5b273eb1f56117e7f7f700b610a2a9
CoderCat010 <nusraumme@gmail.com>
2025-12-16 23:37:19 +0600

    added practice-4

--- JS DAY-4/array.js
+++ JS DAY-4/array.js
@@ -8,3 +8,10 @@
 
 // const num = ['122dcc', 'cnjdncjd', 'njdnjdd'];
 // console.log(num[2]);
+
+
+let num = [1, 3, 3, 4, 5, 6];
+let changeElmByindex = 2;
+num[1] = changeElmByindex;
+console.log(num);
+
