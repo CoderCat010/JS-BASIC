02debf90d48e02c44cc0750963f73eb205894e6b
CoderCat010 <nusraumme@gmail.com>
2026-01-01 22:47:59 +0600

    added practice 3

--- JS DAY-4/array-methods.js
+++ JS DAY-4/array-methods.js
@@ -1,22 +1,20 @@
 // JS METHODS 
 // Data adding methods
 
-// push() // removig from last
+// // push() 
 // let x = [1, 2, 3, 4]
 // console.log(x.push()); 
 
 
-// unshift()
-let x = [1 ,2, 3, 4];
-console.log(x.unshift());
+// // unshift()
+// let x = [1 ,2, 3, 4];
+// console.log(x.unshift());
 
 
+// // pop()
+// let x = [1, 2, 3, 4, 5];
+// console.log(x.pop());
 
-
-
-
-
-// pop()
 
 // shift()
 
