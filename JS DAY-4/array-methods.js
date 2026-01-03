eba0a303b327915fa7d37cc0db55b85fc2556178
CoderCat010 <nusraumme@gmail.com>
2026-01-03 14:33:51 +0600

    added practice-17

--- JS DAY-4/array-methods.js
+++ JS DAY-4/array-methods.js
@@ -7,8 +7,7 @@
 let z = ['apple', 'banana', 'grape', 'kiwi', 'mango', 'apple'];
 console.log(z.sort());
 
-let nums = [10, 2, 5];
-console.log(nums.sort());
+
 
 
 //----------ADDING ELEMMMENT METHOHDS----------
