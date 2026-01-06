05ea8446ccd096ab2b5d35abfac0eaf3abd1e5ed
CoderCat010 <nusraumme@gmail.com>
2026-01-07 01:57:40 +0600

    added practice-3

--- JS DAY-4/array-methods.js
+++ JS DAY-4/array-methods.js
@@ -99,6 +99,12 @@
 
 
 
+// //----------ARRAY CONVERTING METHOHDS----------        
+// // toString
+// const fruits = [1, 2, 3, 4, 5];
+// let myList = fruits.toString(); 
+// console.log(myList);
+// console.log(typeof myList);
 
 
 
