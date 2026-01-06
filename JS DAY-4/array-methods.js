0ba863cbf760bec108aa84c0c498eff14d492da5
CoderCat010 <nusraumme@gmail.com>
2026-01-06 18:22:48 +0600

    deleted unnessecary spaces

--- JS DAY-4/array-methods.js
+++ JS DAY-4/array-methods.js
@@ -5,14 +5,13 @@
 // console.log(x.length);
 
 
-//----------ADDING ELEMMMENT METHOHDS----------
 
+//----------ADDING ELEMMMENT METHOHDS----------
 // // push -- adding from last
 // let x = [1, 2, 3, 4, 5];
 // x.push(6)
 // console.log(x); // puro array ta dekhacce 
 // console.log(x.push(7)); // ekhane x.push mane notun value ta add korbe ar sei sathe notun array er length ta return korbe. 
-
 
 // // unshift --- adding from first
 // let x = [1, 2, 3, 4, 5]; 
@@ -20,15 +19,14 @@
 // console.log(x); // puro array ta dekhacce sathe length o dekhacce
 
 
-//----------REMOVING ELEMMMENT METHOHDS----------
 
+//----------REMOVING ELEMMMENT METHOHDS----------
 // // pop -- removing from last
 // let x = [1, 2, 3, 4, 5];
 // x.pop();
 // console.log(x);
 // console.log(x.pop()); // ekhane x.pop mane last theke ekta value remove korbe ar sei value ta return korbe.
 // console.log(x); // ekhane dekhacce last theke 2 ta value remove hoise tai array te 3 ta value ase.
-
 
 // // shift -- removing from first
 // let x = [1, 2, 3, 4, 5];
@@ -40,12 +38,14 @@
 
 
 //----------SEARCHING ELEMMMENT METHOHDS----------
-
-// // indexOf -- searching element index
+// // indexOf
 // let x = [1, 2, 3, 4, 5];
 // let y = ['abc', 'def', 'ghi', 'jkl'];
 // console.log(x.indexOf(33)); // jodi kono element na thake tahole -1 return korbe
 // console.log(y.indexOf('ghi'));
+
+// // includes 
+
 
 
 
