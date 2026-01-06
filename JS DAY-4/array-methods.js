e0ee386c14d41a641475887ac0ac549036fc26a4
CoderCat010 <nusraumme@gmail.com>
2026-01-07 01:13:52 +0600

    added practice-11

--- JS DAY-4/array-methods.js
+++ JS DAY-4/array-methods.js
@@ -35,6 +35,13 @@
 // console.log(x.shift()); // ekhane x.shift mane first theke ekta value remove korbe ar sei value ta return korbe.
 // console.log(x); // ekhane dekhacce first theke 2 ta value remove hoise tai array te 3 ta value ase.
 
+// slice
+let x = [1, 2, 3, 4, 5];
+console.log(x.slice(1)); // ekhane slice method 1 index theke suru kore array er sesh porjonto element gulo new array hisebe return korbe. tai output hobe 
+
+console.log(x.slice(1, 4)); // ekhane slice method 1 index theke suru kore 4 index er age porjonto element gulo new array hisebe return korbe. tai output hobe [2, 3, 4]
+
+
 
 
 //----------SEARCHING ELEMMMENT METHOHDS----------
@@ -45,11 +52,10 @@
 // console.log(y.indexOf('ghi'));
 
 // // includes 
-let x = [1, 2, 3, 4, 5];
-console.log(x.includes(9));
-let y = ['abc', 'def', 'ghi', 'jkl'];
-console.log(y.includes('e')); // includes method entire element ta check kore, tar moddhe kono part check kore na. tai ekhane false return korbe. jodi 'def' check kortam tahole true return korto karon eta puro ekta element r e hocce element er ekta part o e ke ekta element hishbe dhorce but jeta ekhane check korsi seta puro element na tai false. ba onno vabe bola jay je o puro element ta check kore element er moddhe kono part check kore na.
-
+// let x = [1, 2, 3, 4, 5];
+// console.log(x.includes(9));
+// let y = ['abc', 'def', 'ghi', 'jkl'];
+// console.log(y.includes('e')); // includes method entire element ta check kore, tar moddhe kono part check kore na. tai ekhane false return korbe. jodi 'def' check kortam tahole true return korto karon eta puro ekta element r e hocce element er ekta part o e ke ekta element hishbe dhorce but jeta ekhane check korsi seta puro element na tai false. ba onno vabe bola jay je o puro element ta check kore element er moddhe kono part check kore na.
 
 
 
