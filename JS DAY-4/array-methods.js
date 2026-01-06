1c531929334d496427a2e23553194e99bc64ddf2
CoderCat010 <nusraumme@gmail.com>
2026-01-06 14:21:42 +0600

    added practice-1

--- JS DAY-4/array-methods.js
+++ JS DAY-4/array-methods.js
@@ -1,57 +1,6 @@
-// JS METHODS 
-//-------ADDING METHODS
+//* JS METHODS 
 
-// // push() 
-// let x = [1, 2, 3, 4]
-// console.log(x.push()); 
-
-
-// // unshift()
-// let x = [1 ,2, 3, 4];
-// console.log(x.unshift());
-
-
-//-------DATA REMOVING METHODS
-// // pop()
-// let x = [1, 2, 3, 4, 5];
-// console.log(x.pop());
-
-
-// // shift()
-// let x = [1, 2, 3, 4, 5];
-// console.log(x.shift());
-
-
-// // splice()
-// let x = [1, 2, 3, 4, 5, 6];
-// console.log(x.slice(1, 3));
-
-
-//-----FINDING ELEMENT METHODS
-// // index
-// let x = [1, 2, 3, 4, 5, 6];
-// console.log(x[0]);
-
-
-// // finding from  first
-// let x = [1, 2, 3, 4, 5, 6];
-// console.log(x.indexOf(5)); // nidrishto element ta ache kina seta dekhay thakle tar position dekhay na thakle -1 dekhabe
-
-
-// // finding from last 
-// let x = [1, 2, 3, 4, 5, 6];
-// console.log(x.lastIndexOf(2));
-
-
-// // includes
-// let x = [1, 2, 3, 4, 5, 6];
-// console.log(x.includes(2));
-
-
-
-
-
-
-
-
+// array property
+let x = [1, 2, 3, 4, 5]
+console.log(x.length);
 
