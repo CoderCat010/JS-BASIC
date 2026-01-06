2d7acf28d44304f4b009ab0e4f804699a6ae119f
CoderCat010 <nusraumme@gmail.com>
2026-01-06 14:29:01 +0600

    added practice-2

--- JS DAY-4/array-methods.js
+++ JS DAY-4/array-methods.js
@@ -1,6 +1,15 @@
 //* JS METHODS 
 
-// array property
-let x = [1, 2, 3, 4, 5]
-console.log(x.length);
+// // array property
+// let x = [1, 2, 3, 4, 5]
+// console.log(x.length);
+
+
+//----------ADDING ELEMMMENT METHOHDS----------
+
+// push -- adding from last
+let x = [1, 2, 3, 4, 5];
+x 
+x.push(6)
+console.log(x);
 
