d1c3d428d84efbadf260e5ea2bfb8849c868a7b1
CoderCat010 <nusraumme@gmail.com>
2026-01-10 21:48:30 +0600

    added practice-1

--- JS DAY-5/loop.js
+++ JS DAY-5/loop.js
@@ -1,2 +1,5 @@
-var x = 10;
-console.log(x);
+var forOfLoop = [1, 2, 3, 4, 5];
+for(var value of forOfLoop){
+    console.log('wow!' + value);
+    console.log(forOfLoop);
+} // jodi forofloop de tar mane puro array ta cacchi o loop kore puro array tai barbar dekhabe but jodi value dei tahole er mane hocce forofloop er moddhe je value gulo ache ami segulo ke dekhte cacchi tai o prottekta value loop kore kore kore jabe o dekhabe length er upor depend kore jemon ekhane length 5 o 5 bar loop kore puro array ta dekhabe.
