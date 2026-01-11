279cace852a1050e1b939ab1d76d5e8c9aa0dfa9
CoderCat010 <nusraumme@gmail.com>
2026-01-12 03:59:41 +0600

    added practice-3

--- JS DAY-6/object.js
+++ JS DAY-6/object.js
@@ -21,3 +21,23 @@
 //     }
 // }
 // console.log(battle.anotherObj);
+
+
+const battle = {
+    brand: 'chud ling pong',
+    price: 40,
+    color: 'white',
+    isClean: true,
+    numArr: [1, 2, 3, 4,5],
+    anotherObj: {
+        name: 'cat',
+        color: 'dark',
+        age: 2,
+        owner: 'megh'
+    }
+}
+battle.weight = '2kg';// add new property
+console.log(battle);
+
+
+
