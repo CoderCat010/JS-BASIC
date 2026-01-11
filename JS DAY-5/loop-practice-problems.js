87e84167d58345ee38d7c7bdaef545991636c417
CoderCat010 <nusraumme@gmail.com>
2026-01-11 21:17:41 +0600

    added practice problem-5

--- JS DAY-5/loop-practice-problems.js
+++ JS DAY-5/loop-practice-problems.js
@@ -1,10 +1,10 @@
-// get odd numbers from 1 to 10
+//-----------------get odd numbers from 1 to 10
 // for(var i = 1; i <= 10; i += 2){
 //       console.log(i);   
 // }
 
 
-// get sum of odd numbers from 1 to 10
+//--------------get sum of odd numbers from 1 to 10
 // var sumOfOdd = 0;
 // for(var i = 1; i <= 10; i +=2){
 //     sumOfOdd += i;
@@ -14,17 +14,17 @@
 // console.log(sumOfOdd);
 
 
-// একটি প্রোগ্রাম লিখো যা ১ থেকে ১০ পর্যন্ত প্রতিটি সংখ্যার ঘনফল প্রিন্ট করবে।
+//------------একটি প্রোগ্রাম লিখো যা ১ থেকে ১০ পর্যন্ত প্রতিটি সংখ্যার ঘনফল প্রিন্ট করবে।
 // for(var i = 1; i <= 10; i++){
 //     var cube = i * i * i;
 //     console.log(cube); 
 // }
 
 
-// "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
-for(var i = 1; i <= 60; i++){
-    console.log("I will invest at least 6 hrs every single day for next 60 days!", i);
-}
+//------------"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+// for(var i = 1; i <= 60; i++){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!", i);
+// }
 
 
 
