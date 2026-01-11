b8057e544d31519eeed7b825cb1e9ac5fcdbe5e5
CoderCat010 <nusraumme@gmail.com>
2026-01-11 13:56:55 +0600

    added practice problem-11

--- JS DAY-5/loop-practice-problems.js
+++ JS DAY-5/loop-practice-problems.js
@@ -49,5 +49,18 @@
 // }
 
 
-
-
+// get sumof odd numbers AND sum of even numbers from an array
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 23, 4, 56, 78, 89];
+// let sumOfOdd = 0;
+// let sumOfEven = 0;
+// for(let i = 0; i < numbers.length; i++){
+//     if(numbers[i] % 2 === 0){
+//         sumOfEven += numbers[i];
+//     }else{
+//         sumOfOdd += numbers[i];
+//     }
+    
+    
+// }
+// console.log(sumOfEven);
+// console.log(sumOfOdd);
