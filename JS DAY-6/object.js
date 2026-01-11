161c76020f78f1bcce4993d2713febd7960e9a86
CoderCat010 <nusraumme@gmail.com>
2026-01-12 03:54:51 +0600

    added practice-1

---
+++ JS DAY-6/object.js
@@ -0,0 +1,23 @@
+// const battle = {
+//     brand: 'chud ling pong',
+//     price: 40,
+//     color: 'white',
+//     isClean: true
+// }
+// console.log(battle);
+
+
+const battle = {
+    brand: 'chud ling pong',
+    price: 40,
+    color: 'white',
+    isClean: true,
+    numArr: [1, 2, 3, 4,5],
+    anotherObj: {
+        name: 'cat',
+        color: 'dark',
+        age: 2,
+        owner: 'megh'
+    }
+}
+console.log(battle.anotherObj);
