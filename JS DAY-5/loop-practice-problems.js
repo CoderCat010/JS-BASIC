ebeeafbb49a140ab622dd44024c57f67a817b10c
CoderCat010 <nusraumme@gmail.com>
2026-01-11 20:33:53 +0600

    added loop practice problem-2

--- JS DAY-5/loop-practice-problems.js
+++ JS DAY-5/loop-practice-problems.js
@@ -1,4 +1,18 @@
 // get odd numbers from 1 to 10
-for(var i = 1; i <= 10; i += 2){
-      console.log(i);   
-}
+// for(var i = 1; i <= 10; i += 2){
+//       console.log(i);   
+// }
+
+
+// get sum of odd numbers from 1 to 10
+// var sumOfOdd = 0;
+// for(var i = 1; i <= 10; i +=2){
+//     sumOfOdd += i;
+//      console.log(sumOfOdd);
+// }
+// console.log(i);
+// console.log(sumOfOdd);
+
+
+
+
