020e3850b2b99d1cd9a4b5f8b293ca73ea73a124
CoderCat010 <nusraumme@gmail.com>
2026-01-12 06:26:11 +0600

    added practice-note:2

--- JS DAY-6/keys.js
+++ JS DAY-6/keys.js
@@ -1,9 +1,9 @@
-const battle = {
-    brand: 'chud ling pong',
-    price: 40,
-    color: 'white',
-    isClean: true
-}
+// const battle = {
+//     brand: 'chud ling pong',
+//     price: 40,
+//     color: 'white',
+//     isClean: true
+// }
 // let keys = Object.keys(battle)
 // console.log(Object.keys(battle));
 // console.log(Object.values(battle));
