d86f65223e14269dc10503ff9050f4fedca93281
CoderCat010 <nusraumme@gmail.com>
2026-01-12 07:27:00 +0600

    added practice-11

--- JS DAY-6/object-practice-task.js
+++ JS DAY-6/object-practice-task.js
@@ -6,3 +6,17 @@
 //     "golden rod": '#daa520'
 // };
 // console.log(colors['golden rod']);
+
+
+// For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+// car.passenger = 5;
+// console.log(car);
+
+// or
+// console.log(car.passenger = 5, car);
