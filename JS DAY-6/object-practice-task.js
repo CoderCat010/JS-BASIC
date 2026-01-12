a28dc0d054e8b01a425a8a754f9ae9fd129624fd
CoderCat010 <nusraumme@gmail.com>
2026-01-12 07:29:38 +0600

    added practice-14

--- JS DAY-6/object-practice-task.js
+++ JS DAY-6/object-practice-task.js
@@ -20,3 +20,17 @@
 
 // or
 // console.log(car.passenger = 5, car);
+
+
+
+//* Display the physics marks as output.
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log(student.physics.marks); 
