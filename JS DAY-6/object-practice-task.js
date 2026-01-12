5c0379935090f913186b359506970bc36b60808c
CoderCat010 <nusraumme@gmail.com>
2026-01-12 08:16:18 +0600

    added practice-20

--- JS DAY-6/object-practice-task.js
+++ JS DAY-6/object-practice-task.js
@@ -67,7 +67,7 @@ let myObject = {
 };
 
 for(let checkType in myObject){
-    // console.log(`key: `+ checkType+ ' | '+`type: `+ typeof checkType, `value: `+ myObject[checkType]+ ' | '+`type: `+ typeof myObject[checkType]);
+    console.log(`key: `+ checkType+ ' | '+`type: `+ typeof checkType, `value: `+ myObject[checkType]+ ' | '+`type: `+ typeof myObject[checkType]);
     // or
     console.log(`key: `+ checkType+ ' | '+`type: `+ typeof checkType);
     console.log(`Value: `+ myObject[checkType]+ ' | '+`type: `+ typeof myObject[checkType]);
