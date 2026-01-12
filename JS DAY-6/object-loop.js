2cd7df1625e47209ff89fb27c3e857945b112f02
CoderCat010 <nusraumme@gmail.com>
2026-01-12 07:24:08 +0600

    added practice-9

--- JS DAY-6/object-loop.js
+++ JS DAY-6/object-loop.js
@@ -1,10 +1,10 @@
-const battle = {
-    brand: 'chud ling pong',
-    price: 40,
-    color: 'white',
-    isClean: true,
-}
-for(const property in battle){
-    console.log(property);
-    console.log(battle[property]);
-}
+// const battle = {
+//     brand: 'chud ling pong',
+//     price: 40,
+//     color: 'white',
+//     isClean: true,
+// }
+// for(const property in battle){
+//     console.log(property);
+//     console.log(battle[property]);
+// }
