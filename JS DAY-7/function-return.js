32cad29a27f505268a013b421b06882ee27014fe
CoderCat010 <nusraumme@gmail.com>
2026-01-13 16:04:43 +0600

    added practice-17

--- JS DAY-7/function-return.js
+++ JS DAY-7/function-return.js
@@ -11,3 +11,13 @@
 //     return num1 + num2;
 // }
 // console.log(sumNum(5, 5));
+
+
+function con(number){
+    if(number % 2 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(con(2));
