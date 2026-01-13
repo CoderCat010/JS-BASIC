8620b3c2d19b39a4b4bcbca67a7c720a3c20176f
CoderCat010 <nusraumme@gmail.com>
2026-01-13 15:59:46 +0600

    added practice-16

--- JS DAY-7/function-return.js
+++ JS DAY-7/function-return.js
@@ -7,7 +7,7 @@
 // console.log(returnValues(4));
 
 
-function sumNum(num1, num2){
-    return num1 + num2;
-}
-console.log(sumNum(5, 5));
+// function sumNum(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(sumNum(5, 5));
