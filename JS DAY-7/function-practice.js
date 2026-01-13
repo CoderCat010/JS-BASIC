908a4547f3b70455c00dffc19bdfa2296650f981
CoderCat010 <nusraumme@gmail.com>
2026-01-13 18:09:40 +0600

    added practice-23

--- JS DAY-7/function-practice.js
+++ JS DAY-7/function-practice.js
@@ -6,5 +6,14 @@ console.log(multiplyNumbers(1, 2, 3, 4));
 */
 
 
-
+//-----> Task-2: Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+/* function checkEvenOdd(numbers){
+    if(numbers % 2 === 0){
+        return numbers * 2;
+    }else{
+        return numbers / 2;
+    }
+}
+console.log(checkEvenOdd(2));
+*/
 
