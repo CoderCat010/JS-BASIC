fa859a59d2002f86ceff6e2d072238ab34966d72
CoderCat010 <nusraumme@gmail.com>
2026-01-13 18:09:14 +0600

    added practice-22

---
+++ JS DAY-7/function-practice.js
@@ -0,0 +1,10 @@
+//-----> Task-1: Take four parameters. Multiply the four numbers and then return the result.
+/* function multiplyNumbers(num1, num2, num3, num4){
+    return num1 * num2 * num3 * num4;
+}
+console.log(multiplyNumbers(1, 2, 3, 4));
+*/
+
+
+
+
